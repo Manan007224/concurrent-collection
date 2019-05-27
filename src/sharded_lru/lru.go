c163af13373e70fefd95003844a746952c600543
Manan Maniyar <maniyarmanan1996@gmail.com>
2019-05-27 02:53:12 -0700

    added the hash-function

--- src/sharded_lru/lru.go
+++ src/sharded_lru/lru.go
@@ -50,3 +50,84 @@ func New(opt string, cap int) *LRU {
 	return l
 }
 
+// this initializes some fields at first use. Helpful to
+// allow us to use the empty value of LRU
+func (this *LRU) lazyInit() {
+	if this.shards == nil {
+		this.nshards = 1
+		this.shards = []*shard{newShard(this.cap)}
+	}
+}
+
+func (this *LRU) Len() int {
+	this.lazyInit()
+	var len int
+	for i := 0; i < this.nshards; i++ {
+		len += this.shards[i].Len()
+	}
+	return len
+}
+
+func (l *LRU) shard(key interface{}) *shard {
+	h := fnv.New32a() // used to hash a byte array
+
+	// try to get a bytes representation of the key any way we can, in order
+	// from fastest to slowest
+	switch v := key.(type) {
+	case []byte:
+		h.Write(v)
+	case byter:
+		h.Write(v.Bytes())
+	case string:
+		h.Write([]byte(v))
+	case stringer:
+		h.Write([]byte(v.String()))
+	case int:
+		h.Write(intBytes(v))
+	case *int:
+		h.Write(intBytes(*v))
+	case *bool, bool, []bool, *int8, int8, []int8, *uint8,
+		uint8, *int16, int16, []int16, *uint16,
+		uint16, []uint16, *int32, int32, []int32, *uint32, uint32, []uint32,
+		*int64, int64, []int64, *uint64, uint64, []uint64:
+		h.Write(toBytes(v))
+	default:
+		// the user is using an unknown type as the key, so we're now grasping
+		// at straws here. This will be at least an order of magnitude slower
+		// then the options above.
+		var buf bytes.Buffer
+		enc := gob.NewEncoder(&buf)
+		err := enc.Encode(v)
+		if err != nil {
+			panic(fmt.Sprintf("could not encode type %T as bytes", key))
+		}
+		h.Write(buf.Bytes())
+	}
+
+	return l.shards[h.Sum32()&uint32(l.nshards-1)]
+}
+
+func toBytes(v interface{}) []byte {
+	var buf bytes.Buffer
+	if err := binary.Write(&buf, binary.LittleEndian, v); err != nil {
+		panic(fmt.Sprintf("could not encode %v as bytes: %v", v, err))
+	}
+	return buf.Bytes()
+}
+
+var il = strconv.IntSize / 8
+
+// helper function to quickly turn an int into a byte slice
+func intBytes(i int) []byte {
+	b := make([]byte, il)
+	b[0] = byte(i)
+	b[1] = byte(i >> 8)
+	b[2] = byte(i >> 16)
+	b[3] = byte(i >> 24)
+	if il == 8 {
+		b[4] = byte(i >> 32)
+		b[5] = byte(i >> 40)
+		b[6] = byte(i >> 48)
+		b[7] = byte(i >> 56)
+	}
+	return b
